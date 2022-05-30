cbecf10a7303ba4c9d7d3a2aa179654194b5e158
Sirjamo1 <jam_how@hotmail.com>
2022-05-31 06:16:42 +1000

    fix bug with not overlapping grids by making one function that creates a a number of row divs then creates another number of column divs to append to each div

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,18 +1,18 @@
-const container = document.getElementById('gridContainer');
+const container = document.getElementById("gridContainer");
 
-rowCreator(16);
-columnCreator(16);
-const rowDiv = document.getElementById('rowDiv');
-function rowCreator(rowNum) {
- for (let r = 0; r < rowNum; r++) {
-  let row = document.createElement('div');
-  rowDiv.appendChild(row).className = 'rows';
- };
-};
-const columnDiv = document.getElementById('columnDiv');
-function columnCreator(columnNum) {
- for (let c = 0; c < columnNum; c++) {
-  let column = document.createElement('div');
-  columnDiv.appendChild(column).className = 'columns';
- };
-};
+gridCreator(16, 16);
+
+// creates a number(1st arg) of divs and appends them to the container
+// then creates a number(2nd arg) of divs and appends them to each of the 1st arg divs
+function gridCreator(rowNum, colNum) {
+  for (let r = 0; r < rowNum; r++) {
+    let row = document.createElement("div");
+    container.appendChild(row).className = "rows";
+    const rowContainer = document.getElementsByClassName("rows");
+    for (let c = 0; c < colNum; c++) {
+      let col = document.createElement("div");
+      rowContainer[r].appendChild(col).className = "cols";
+    }
+  }
+}
+
