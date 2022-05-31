8b5c90b30b7a5cedfb03ea64e5d0507a2d9e1980
Sirjamo1 <jam_how@hotmail.com>
2022-06-01 05:29:28 +1000

    added mouseover event listener which also calls the colorRan function to change the background of the div which the mouse is over

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,15 +1,15 @@
 const container = document.getElementById("gridContainer");
 
-gridCreator(16, 16);
+gridCreator(16);
 
-// creates a number(1st arg) of divs and appends them to the container
-// then creates a number(2nd arg) of divs and appends them to each of the 1st arg divs
-function gridCreator(colNum, singleBoxNum) {
-  for (let c = 0; c < colNum; c++) {
+// creates a number(arg) of divs and appends them to the container
+// then creates a number(arg) of divs and appends them to each of the 1st arg divs
+function gridCreator(num) {
+  for (let c = 0; c < num; c++) {
     let col = document.createElement("div");
     container.appendChild(col).className = "cols";
     const colContainer = document.getElementsByClassName("cols");
-    for (let s = 0; s < singleBoxNum; s++) {
+    for (let s = 0; s < num; s++) {
       let singleBox = document.createElement("div");
       //listens for mouseover event then changes that div to a random color with the colorRan function
       singleBox.addEventListener("mouseover", () => {
